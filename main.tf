8cf741ce1dc3dac1f556672be6d8be133e812ea8
unknown <ivancvitkovic98@gmail.com>
2022-02-09 11:31:30 +0100

    added lock hcl and updated configuration

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
-    }
+      source = "kreuzwerker/docker"
+    version = ">= 2.13.0" }
   }
 }
 
