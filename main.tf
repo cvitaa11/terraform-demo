022bc99a702e9f07102b12571045e91edb9305de
unknown <ivancvitkovic98@gmail.com>
2022-02-10 10:08:04 +0100

    Fixed variable name

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = var.containe_name
+  name  = var.container_name
   ports {
     internal = 80
     external = 8080
